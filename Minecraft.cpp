536243d485026c57507f29822d1c35a490c5048f
Hyun-38 <155708154+Hyun-38@users.noreply.github.com>
2025-01-27 17:24:08 +0900

    2025-01-27

---
+++ Minecraft.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int
+
+    int
+    main()
+{
+  int N, M, B;
+  cin >> N >> M >> B;
+
+  for (int i = 1; i < N; i++)
+  {
+    for (int j = 1; j < M; j++)
+    {
+    }
+  }
+}
---
+++ Welcome.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int S, M, L, XL, XXL, XXXL, NUM;
+  cin >> num;
+  cin >> S >> M >> L >> XL >> XXL >> XXXL;
+}
