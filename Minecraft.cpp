e8c591b2f12595ab5e9d081a7d70879680a6be96
Hyun-38 <155708154+Hyun-38@users.noreply.github.com>
2025-02-01 14:49:52 +0900

    2025-02-01

--- Minecraft.cpp
+++ Minecraft.cpp
@@ -1,19 +1,56 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
 
 using namespace std;
 
-int
-
-    int
-    main()
+int main()
 {
   int N, M, B;
   cin >> N >> M >> B;
 
-  for (int i = 1; i < N; i++)
+  vector<int> land(N * M);
+  int minHeight = 256, maxHeight = 0;
+
+  for (int i = 0; i < N * M; i++)
   {
-    for (int j = 1; j < M; j++)
+    cin >> land[i];
+    minHeight = min(minHeight, land[i]);
+    maxHeight = max(maxHeight, land[i]);
+  }
+
+  int minTime = INT_MAX;
+  int bestHeight = 0;
+
+  for (int h = minHeight; h <= maxHeight; h++)
+  {
+    int removeBlocks = 0, addBlocks = 0;
+
+    for (int i = 0; i < N * M; i++)
     {
+      if (land[i] > h)
+      {
+        removeBlocks += (land[i] - h);
+      }
+      else if (land[i] < h)
+      {
+        addBlocks += (h - land[i]);
+      }
+    }
+
+    if (removeBlocks + B >= addBlocks)
+    {
+      int time = (removeBlocks * 2) + addBlocks;
+
+      if (time <= minTime)
+      {
+        minTime = time;
+        bestHeight = h;
+      }
     }
   }
+
+  cout << minTime << " " << bestHeight << endl;
+  return 0;
 }
