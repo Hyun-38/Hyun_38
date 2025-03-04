41936dc1e4eb7d81f810104f4da341a99dea7fc1
Hyun-38 <155708154+Hyun-38@users.noreply.github.com>
2025-03-04 23:23:49 +0900

    2025-03-04

--- Hashing.cpp
+++ Hashing.cpp
@@ -1,8 +1,32 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int main()
 {
-  
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  int N, M, num;
+  cin >> N;
+
+  vector<int> arr1(N);
+  for (int i = 0; i < N; i++)
+  {
+    cin >> arr1[i];
+  }
+
+  sort(arr1.begin(), arr1.end());
+
+  cin >> M;
+  while (M--)
+  {
+    cin >> num;
+    cout << (binary_search(arr1.begin(), arr1.end(), num) ? "1\n" : "0\n");
+  }
+
+  return 0;
 }
