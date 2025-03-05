ac5be1c37d680dd2260fc5dc83f27abddba1212b
Hyun-38 <155708154+Hyun-38@users.noreply.github.com>
2025-03-05 15:03:49 +0900

    2025-03-05

---
+++ Lansun.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+  int K, N, num;
+  cin >> K >> N;
+  vector<int> v;
+  while (K--)
+  {
+    cin >> num;
+    v.push_back(num);
+  }
+  
+}
