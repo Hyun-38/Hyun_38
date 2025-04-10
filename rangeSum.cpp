c876876f0d2b2bec9fb4e99fbe08d790ce462ca6
Hyun-38 <155708154+Hyun-38@users.noreply.github.com>
2025-04-10 13:58:39 +0900

    2025-04-10

--- rangeSum.cpp
+++ rangeSum.cpp
@@ -3,23 +3,24 @@
 using namespace std;
 
 int main(){
-  ios::sync_with_stdio(0);
+  ios::sync_with_stdio(false);
   cin.tie(0);
+  
   int N, M;
   cin >> N >> M;
 
-  vector<int> v(N);
-  for (int i = 0; i < N; i++){
-    cin >> v[i];  
+  vector<int> v(N + 1);      
+  vector<int> Sum(N + 1, 0);
+
+  for (int i = 1; i <= N; i++) {
+    cin >> v[i];
+    Sum[i] = Sum[i - 1] + v[i];
   }
 
-  for (int i = 0; i < M; i++){
-    int num1, num2, sum = 0;
+  int num1, num2;
+  for (int i = 0; i < M; i++) {
     cin >> num1 >> num2;
-    for (int j = num1 - 1; j <= num2 - 1; j++){
-      sum += v[j];
-    }
-    cout << sum << "\n";
+    cout << Sum[num2] - Sum[num1 - 1] << "\n";
   }
 
   return 0;
